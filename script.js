0fe1bf7c269fb789f5e90fc02ba0d2120eb24117
Nadya <rozy93@bk.ru>
2023-11-13 13:35:22 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+
+var swiper = new Swiper(".plants__swiper", {
+    slidesPerView: 3.5,
+    spaceBetween: 30,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+  });
+
+  let ms = Date.now();
+  console.log(ms);
+
+
